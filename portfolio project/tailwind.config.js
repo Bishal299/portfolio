2769cf88a6febe4c7b1aa3df35ba0112043e152c
Bishal299 <bishalkhuntia23@gmail.com>
2025-07-16 22:42:56 +0530

    Add files via upload

---
+++ portfolio project/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FF5733', // Example custom color
+      },
+      width: {
+        '84': '21rem',
+      },
+      height: {
+        '84': '21rem',
+      },
+      borderRadius: {
+        'half': '50%',
+      },
+    },
+  },
+  plugins: [],
+}
